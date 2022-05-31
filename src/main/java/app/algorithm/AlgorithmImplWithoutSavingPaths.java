cd3712dee790c9b1338727d8a70c99c140f323be
Raz Cohen <razcohenofficial@gmail.com>
2022-05-31 20:37:38 +0300

    Added total score to AlgorithmImplWithoutSavingPaths

--- src/main/java/app/algorithm/AlgorithmImplWithoutSavingPaths.java
+++ src/main/java/app/algorithm/AlgorithmImplWithoutSavingPaths.java
@@ -1,9 +1,6 @@
 package app.algorithm;
 
-import app.model.PathDetails;
-import app.model.ProblemInput;
-import app.model.ProblemOutput;
-import app.model.Street;
+import app.model.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -21,17 +18,21 @@ public class AlgorithmImplWithoutSavingPaths implements Algorithm {
     // in both directions gives points
     @Override
     public ProblemOutput run(ProblemInput problemInput) {
+        List<PathDetails> bestPaths = new ArrayList<>();
         List<List<Street>> bestCarsPaths = new ArrayList<>();
         List<Street> traversedAlreadyStreets = new ArrayList<>();
         for (int carIndex = 0; carIndex < problemInput.getMissionProperties().getAmountOfCars(); carIndex++) {
             PathDetails bestPath = bestPathFinder.findBestPath(problemInput, traversedAlreadyStreets);
             bestCarsPaths.add(bestPath.getStreets());
+            bestPaths.add(bestPath);
             traversedAlreadyStreets.addAll(bestPath.getStreets());
             bestPath.getStreets().forEach(street -> {
                 if (!street.isOneway())
                     traversedAlreadyStreets.add(problemInput.getStreetToInverseStreet().get(street));
             });
         }
+        double totalScore = bestPaths.stream().map(PathDetails::getScore).reduce((double) 0, Double::sum);
+        System.out.println(totalScore);
         return ProblemOutput.builder().bestCarsPaths(bestCarsPaths).build();
     }
 }
