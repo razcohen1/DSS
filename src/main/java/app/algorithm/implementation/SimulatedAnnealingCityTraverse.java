de72a3d2afae3ed60d61445ae1f47f5a5945e4ac
Raz Cohen <razcohenofficial@gmail.com>
2022-06-05 19:14:46 +0300

    Created property for restarts field in Hill climbing algorithm

--- src/main/java/app/algorithm/implementation/SimulatedAnnealingCityTraverse.java
+++ src/main/java/app/algorithm/implementation/SimulatedAnnealingCityTraverse.java
@@ -18,26 +18,26 @@ import static app.algorithm.services.StreetsScorer.calculateTotalScore;
 import static app.algorithm.services.StreetsScorer.getZeroScoreStreetsFromPath;
 
 @Service
-@ConditionalOnProperty(value = "algorithm.implementation", havingValue = "sa")
+@ConditionalOnProperty(value = "algorithm.implementation", havingValue = "hillclimbing")
 public class SimulatedAnnealingCityTraverse implements CityTraverseAlgorithm {
     @Value(value = "${maximum.running.time.wanted.in.seconds:30}")
     private double maximumRunningTime;
+    @Value(value = "${number.of.restarts:100}")
+    private int numberOfRestarts;
     private SimulatedAnnealingMaximumScorePathFinder pathFinder = new SimulatedAnnealingMaximumScorePathFinder();
-    private int reruns = 100;
 
     @Override
     public ProblemOutput run(ProblemInput problemInput) {
         Map<Street, Street> streetToInverseStreet = problemInput.getStreetToInverseStreet();
         List<Path> bestPaths = new ArrayList<>();
         List<Street> zeroScoreStreets = new ArrayList<>();
-//        problemInput.getMissionProperties().setTimeAllowedForCarsItinerariesInSeconds(10000);
-        pathFinder.setMaximumRunningTimeInSeconds(maximumRunningTime / (problemInput.getMissionProperties().getAmountOfCars() * reruns));
+        pathFinder.setMaximumRunningTimeInSeconds(maximumRunningTime / (problemInput.getMissionProperties().getAmountOfCars() * numberOfRestarts));
         Path bestPath = Path.builder().score(0).build();
         problemInput.setZeroScoreStreets(zeroScoreStreets);
         for (int carIndex = 0; carIndex < problemInput.getMissionProperties().getAmountOfCars(); carIndex++) {
             bestPath = Path.builder().score(0).build();
 //            problemInput.setZeroScoreStreets(new ArrayList<>(zeroScoreStreets));
-            for (int rerunIndex = 0; rerunIndex < reruns; rerunIndex++) {
+            for (int rerunIndex = 0; rerunIndex < numberOfRestarts; rerunIndex++) {
                 Path path = pathFinder.find(problemInput);
                 if (path.getScore() > bestPath.getScore())
                     bestPath = path;
